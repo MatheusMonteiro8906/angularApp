43eac8d351929ffe13bc017d59aec6232e0d47bc
Matheus Monteiro <monteiromatheus047@gmail.com>
2024-10-30 22:16:55 -0300

    chore: first Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angularApp' title`, () => {
+  it(`should have the 'Matheus Monteiro' name`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularApp');
+    expect(app.ownerName).toEqual('Matheus Monteiro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angularApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Olá, Matheus Monteiro');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angularApp';
+export class AppComponent implements OnInit, AfterViewInit{
+  constructor() { }
+
+  ngOnInit(): void {
+      console.log("OnInit called")
+  }
+
+  ngAfterViewInit(): void {
+      console.log("AfterViewInit called")
+  }
+  
+  ownerName = 'Matheus Monteiro';
 }
