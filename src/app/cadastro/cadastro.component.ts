3d8b28bb9395320829b06a820026be9399dd1098
Matheus Monteiro <monteiromatheus047@gmail.com>
2024-11-06 22:19:14 -0300

    update: improved book add form and site overall style

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
+export class CadastroComponent{
+
+  constructor(private _router: Router) { }
+
+  AddBookForm = new FormGroup({    
+    Name: new FormControl('', Validators.required),
+    Synopsis: new FormControl('', Validators.required),
+    Rating: new FormControl('', [Validators.required, Validators.min(0), Validators.max(5)]),
+  })
+
+  onSubmit(){
+    console.log(this.AddBookForm.value);
+
+    if(Number(this.AddBookForm.value.Rating) == 3){
+      alert("Rating must not be 3");
+    } else {
+      this._router.navigate(['livros']);
+      // alert("Livro adicionado com sucesso");
+    }
+  }
 
 }
