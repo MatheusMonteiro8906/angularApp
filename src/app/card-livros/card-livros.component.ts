5ade1c1718fa8cc02aecc24faca306778784c2fd
Matheus Monteiro <monteiromatheus047@gmail.com>
2024-11-07 18:39:19 -0300

    update: improved book list and started new style pattern

--- src/app/card-livros/card-livros.component.ts
+++ src/app/card-livros/card-livros.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { IBook } from '../../entities/Book';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-card-livros',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, NgFor],
   templateUrl: './card-livros.component.html',
   styleUrl: './card-livros.component.css'
 })
