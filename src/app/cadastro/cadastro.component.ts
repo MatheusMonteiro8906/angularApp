f6bec9491ff373398a89c54454e9d78948c733fc
Matheus Monteiro <monteiromatheus047@gmail.com>
2024-11-08 21:45:17 -0300

    update: added environment files and refactored code with mat

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
@@ -14,15 +18,15 @@ export class CadastroComponent{
   constructor(private _router: Router) { }
 
   AddBookForm = new FormGroup({    
-    Name: new FormControl('', Validators.required),
-    Synopsis: new FormControl('', Validators.required),
-    Rating: new FormControl('', [Validators.required, Validators.min(0), Validators.max(5)]),
+    name: new FormControl('', Validators.required),
+    synopsis: new FormControl('', Validators.required),
+    rating: new FormControl('', [Validators.required, Validators.min(0), Validators.max(5)]),
   })
 
   onSubmit(){
     console.log(this.AddBookForm.value);
 
-    if(Number(this.AddBookForm.value.Rating) == 3){
+    if(Number(this.AddBookForm.value.rating) == 3){
       alert("Rating must not be 3");
     } else {
       this._router.navigate(['livros']);
